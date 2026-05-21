8a03fe8517b361f9ba7bd38a091a28899ef0d3ac
Jack Blower <Jack@elvenspellmaker.co.uk>
2026-05-21 16:27:34 +0100

    feat: implement strict flag
     * Implements a strict flag for v2
     * Prints out errors for failing keys or JSON path elements
     * Adds Commit Lint to the builds
     * Adds GitVersion.yaml for Conventional Commit style bumps

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	cfgmgr "github.com/DevLabFoundry/configmanager/v2/cmd/configmanager"
 	"github.com/DevLabFoundry/configmanager/v2/internal/log"
 )
 
+const redTerminal = "\x1b[31m%s\x1b[0m"
+
 func main() {
 	logger := log.New(os.Stderr)
 	cmd := cfgmgr.NewRootCmd(logger)
 	if err := cmd.Execute(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, redTerminal+"\n", err)
 		os.Exit(1)
 	}
 }
